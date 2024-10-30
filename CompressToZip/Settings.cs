b1b9cc32af8e3478edda83426d10bb1ad59c4a64
Ethan James Beal <21134925+HDeDeDe@users.noreply.github.com>
2024-10-30 14:48:28 -0700

    update Settings.cs

--- CompressToZip/Settings.cs
+++ CompressToZip/Settings.cs
@@ -4,19 +4,19 @@ internal static class Settings {
 //-----------------------------------------------------Customize--------------------------------------------------------
 	// ReSharper disable once InconsistentNaming
 	public const bool giveMePDBs = true;
-	public const bool weave = false;
+	public const bool weave = true;
 
 	public const string pluginName = HDeMods.InterlopingArtifactPlugin.PluginName;
 	public const string pluginAuthor = HDeMods.InterlopingArtifactPlugin.PluginAuthor;
 	public const string pluginVersion = HDeMods.InterlopingArtifactPlugin.PluginVersion;
 	public const string changelog = "../CHANGELOG.md";
 	public const string readme = "../README.md";
-	public const string icon = "../Resources/icon.png";
+	public const string icon = "";
 
 	public const string riskOfRain2Install =
 		@"C:\Program Files (x86)\Steam\steamapps\common\Risk of Rain 2\Risk of Rain 2_Data\Managed\";
 
-	public static readonly ArrayList extraFiles = new ArrayList {
+	public static ArrayList extraFiles = new ArrayList {
 		new FileInfo("../Resources/InterlopingSounds/GeneratedSoundBanks/Windows/Inter_TickTock.bnk")
 	};
 

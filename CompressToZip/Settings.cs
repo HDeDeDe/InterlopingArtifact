618d3333d5c65d4cace5cd87195eda6348ad2a1e
Ethan James Beal <21134925+HDeDeDe@users.noreply.github.com>
2024-11-03 00:07:55 -0700

    added icon.png

--- CompressToZip/Settings.cs
+++ CompressToZip/Settings.cs
@@ -11,7 +11,7 @@ internal static class Settings {
 	public const string pluginVersion = HDeMods.InterlopingArtifactPlugin.PluginVersion;
 	public const string changelog = "../CHANGELOG.md";
 	public const string readme = "../README.md";
-	public const string icon = "";
+	public const string icon = "../Resources/icon.png";
 
 	public const string riskOfRain2Install =
 		@"C:\Program Files (x86)\Steam\steamapps\common\Risk of Rain 2\Risk of Rain 2_Data\Managed\";
@@ -22,8 +22,8 @@ internal static class Settings {
 		new FileInfo("../Resources/intericons")
 	};
 
-	public const string manifestWebsiteUrl = "";
-	public const string manifestDescription = "";
+	public const string manifestWebsiteUrl = "https://github.com/HDeDeDe/InterlopingArtifact";
+	public const string manifestDescription = "Enemies will begin to swarm you after a period of time.";
 
 	public const string manifestDependencies = "[\n" +
 	                                           "\t\t\"bbepis-BepInExPack-5.4.2108\",\n" +
@@ -32,6 +32,7 @@ internal static class Settings {
 	                                           "\t\t\"RiskofThunder-R2API_Networking-1.0.2\",\n" +
 	                                           "\t\t\"RiskofThunder-R2API_Prefab-1.0.4\",\n" +
 	                                           "\t\t\"RiskofThunder-R2API_Sound-1.0.3\",\n" +
+	                                           "\t\t\"RiskofThunder-R2API_ArtifactCode-1.0.1\",\n" +
 	                                           "\t\t\"RiskofThunder-R2API_Director-2.3.1\"\n" +
 	                                           "\t]";
 }

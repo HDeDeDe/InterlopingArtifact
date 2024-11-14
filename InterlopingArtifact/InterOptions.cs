655d35f9bb9c178686fd5f02e9f5baf187b99de6
Ethan James Beal <21134925+HDeDeDe@users.noreply.github.com>
2024-11-13 17:34:43 -0800

    update RoO to match the one in ChunkyMode

--- InterlopingArtifact/InterOptions.cs
+++ InterlopingArtifact/InterOptions.cs
@@ -1,88 +1,141 @@
+using System.Diagnostics.CodeAnalysis;
 using RiskOfOptions.OptionConfigs;
 using BepInEx.Configuration;
 using RiskOfOptions.Options;
 using RiskOfOptions;
 using System.Runtime.CompilerServices;
+using HarmonyLib;
+using RiskOfOptions.Components.Options;
+using RiskOfOptions.Components.Panel;
 using UnityEngine;
 using UnityEngine.Events;
 
 namespace HDeMods {
     namespace InterOptionalMods {
+        [SuppressMessage("ReSharper", "InconsistentNaming")]
+        [SuppressMessage("ReSharper", "StringLiteralTypo")]
         internal static class RoO {
             public static bool Enabled =>
                 BepInEx.Bootstrap.Chainloader.PluginInfos.ContainsKey("com.rune580.riskofoptions");
 
+            public delegate void LogFunc(object data);
+
+            private static string modGUID;
+            private static string modNAME;
+            private static LogFunc logError;
+            private static bool initialized;
+#if DEBUG
+            private static LogFunc logDebug;
+#endif
+
+            public static void Init(string modGuid, string modName, LogFunc errorFunc, LogFunc debugFunc = null) {
+#if DEBUG
+                logDebug = debugFunc;
+#endif
+                logError = errorFunc;
+                modGUID = modGuid;
+                modNAME = modName;
+                if (!Enabled) {
+                    logError("Risk of Options is not present, the author of " + modNAME +
+                             " did not check for this! Mod GUID: " + modGUID);
+                    return;
+                }
+                initialized = true;
+            }
+
             [MethodImpl(MethodImplOptions.NoInlining | MethodImplOptions.NoOptimization)]
             public static void AddCheck(ConfigEntry<bool> option, bool requireRestart = false) {
+                if (!initialized) return;
                 LocalizedCheckBoxOption boxOption = new LocalizedCheckBoxOption(option, requireRestart);
-                ModSettingsManager.AddOption(boxOption, InterlopingArtifactPlugin.PluginGUID,
-                    InterlopingArtifactPlugin.PluginName);
+                ModSettingsManager.AddOption(boxOption, modGUID,
+                    modNAME);
 #if DEBUG
-                INTER.Log.Debug(boxOption.GetNameToken());
-                INTER.Log.Debug(boxOption.GetDescriptionToken());
+                if (logDebug == null) return;
+                logDebug(boxOption.GetNameToken());
+                logDebug(boxOption.GetDescriptionToken());
 #endif
             }
 
             [MethodImpl(MethodImplOptions.NoInlining | MethodImplOptions.NoOptimization)]
             public static void AddInt(ConfigEntry<int> option, int minimum, int maximum) {
+                if (!initialized) return;
                 LocalizedIntSliderOption sliderOption =
                     new LocalizedIntSliderOption(option, new IntSliderConfig() { min = minimum, max = maximum });
-                ModSettingsManager.AddOption(sliderOption, InterlopingArtifactPlugin.PluginGUID,
-                    InterlopingArtifactPlugin.PluginName);
-
+                ModSettingsManager.AddOption(sliderOption, modGUID,
+                    modNAME);
 #if DEBUG
-                INTER.Log.Debug(sliderOption.GetNameToken());
-                INTER.Log.Debug(sliderOption.GetDescriptionToken());
+                if (logDebug == null) return;
+                logDebug(sliderOption.GetNameToken());
+                logDebug(sliderOption.GetDescriptionToken());
 #endif
             }
 
             [MethodImpl(MethodImplOptions.NoInlining | MethodImplOptions.NoOptimization)]
             public static void AddFloat(ConfigEntry<float> option, float minimum, float maximum,
                 string format = "{0:0}%") {
+                if (!initialized) return;
                 LocalizedSliderOption sliderOption = new LocalizedSliderOption(option,
                     new SliderConfig() { min = minimum, max = maximum, FormatString = format });
-                ModSettingsManager.AddOption(sliderOption, InterlopingArtifactPlugin.PluginGUID,
-                    InterlopingArtifactPlugin.PluginName);
-
+                ModSettingsManager.AddOption(sliderOption, modGUID,
+                    modNAME);
 #if DEBUG
-                INTER.Log.Debug(sliderOption.GetNameToken());
-                INTER.Log.Debug(sliderOption.GetDescriptionToken());
+                if (logDebug == null) return;
+                logDebug(sliderOption.GetNameToken());
+                logDebug(sliderOption.GetDescriptionToken());
 #endif
             }
 
             [MethodImpl(MethodImplOptions.NoInlining | MethodImplOptions.NoOptimization)]
             public static void AddFloatStep(ConfigEntry<float> option, float minimum, float maximum, float step,
                 string format = "{0:0}%") {
-                LocalizedSliderStepOption stepSliderOption = new LocalizedSliderStepOption(option, new StepSliderConfig()
-                    { min = minimum, max = maximum, FormatString = format, increment = step });
-                ModSettingsManager.AddOption(stepSliderOption, InterlopingArtifactPlugin.PluginGUID,
-                    InterlopingArtifactPlugin.PluginName);
-
+                if (!initialized) return;
+                LocalizedSliderStepOption stepSliderOption = new LocalizedSliderStepOption(option,
+                    new StepSliderConfig()
+                        { min = minimum, max = maximum, FormatString = format, increment = step });
+                ModSettingsManager.AddOption(stepSliderOption, modGUID,
+                    modNAME);
 #if DEBUG
-                INTER.Log.Debug(stepSliderOption.GetNameToken());
-                INTER.Log.Debug(stepSliderOption.GetDescriptionToken());
+                if (logDebug == null) return;
+                logDebug(stepSliderOption.GetNameToken());
+                logDebug(stepSliderOption.GetDescriptionToken());
 #endif
             }
-            
+
             [MethodImpl(MethodImplOptions.NoInlining | MethodImplOptions.NoOptimization)]
             public static void AddButton(string name, string category, UnityAction onButtonPressed) {
+                if (!initialized) return;
                 LocalizedButtonOption buttonOption = new LocalizedButtonOption(name, category, "", "", onButtonPressed);
-                ModSettingsManager.AddOption(buttonOption, InterlopingArtifactPlugin.PluginGUID,
-                    InterlopingArtifactPlugin.PluginName);
-
+                ModSettingsManager.AddOption(buttonOption, modGUID,
+                    modNAME);
 #if DEBUG
-                INTER.Log.Debug(buttonOption.GetNameToken());
-                INTER.Log.Debug(buttonOption.GetDescriptionToken());
-                INTER.Log.Debug(buttonOption.GetButtonLabelToken());
+                if (logDebug == null) return;
+                logDebug(buttonOption.GetNameToken());
+                logDebug(buttonOption.GetDescriptionToken());
+                logDebug(buttonOption.GetButtonLabelToken());
 #endif
             }
 
             [MethodImpl(MethodImplOptions.NoInlining | MethodImplOptions.NoOptimization)]
-            public static void SetSprite(Sprite sprite) => ModSettingsManager.SetModIcon(sprite);
+            public static void SetSprite(Sprite sprite) {
+                ModSettingsManager.SetModIcon(sprite);
+            }
 
             [MethodImpl(MethodImplOptions.NoInlining | MethodImplOptions.NoOptimization)]
-            public static void SetDescriptionToken(string description) => 
+            public static void SetDescriptionToken(string description) {
                 ModSettingsManager.SetModDescriptionToken(description);
+            }
+
+            [MethodImpl(MethodImplOptions.NoInlining | MethodImplOptions.NoOptimization)]
+            public static void ResetToDefault() {
+                if (!initialized) return;
+                ModOptionPanelController options =
+                    // I'm too lazy to find a proper way of doing this
+                    GameObject.Find("SettingsPanelTitle(Clone)").GetComponent<ModOptionPanelController>();
+                foreach (ModSetting setting in options._modSettings) {
+                    if (setting.GetType() == typeof(GenericButtonController)) continue;
+                    AccessTools.Method(setting.GetType(), "ResetToDefault")?.Invoke(setting, null);
+                }
+            }
         }
     }
 
@@ -106,7 +159,8 @@ namespace HDeMods {
 
     internal class LocalizedIntSliderOption : IntSliderOption {
         [MethodImpl(MethodImplOptions.NoInlining | MethodImplOptions.NoOptimization)]
-        public LocalizedIntSliderOption(ConfigEntry<int> configEntry, IntSliderConfig config) : base(configEntry, config) {
+        public LocalizedIntSliderOption(ConfigEntry<int> configEntry, IntSliderConfig config) : base(configEntry,
+            config) {
             RoR2.Language.onCurrentLanguageChanged += ResetDescription;
         }
 
@@ -114,6 +168,7 @@ namespace HDeMods {
         public override void RegisterTokens() {
             Description = RoR2.Language.GetString(GetDescriptionToken());
         }
+
         [MethodImpl(MethodImplOptions.NoInlining | MethodImplOptions.NoOptimization)]
         private void ResetDescription() {
             Description = RoR2.Language.GetString(GetDescriptionToken());
@@ -125,10 +180,12 @@ namespace HDeMods {
         public LocalizedSliderOption(ConfigEntry<float> configEntry, SliderConfig config) : base(configEntry, config) {
             RoR2.Language.onCurrentLanguageChanged += ResetDescription;
         }
+
         [MethodImpl(MethodImplOptions.NoInlining | MethodImplOptions.NoOptimization)]
         public override void RegisterTokens() {
             Description = RoR2.Language.GetString(GetDescriptionToken());
         }
+
         [MethodImpl(MethodImplOptions.NoInlining | MethodImplOptions.NoOptimization)]
         private void ResetDescription() {
             Description = RoR2.Language.GetString(GetDescriptionToken());
@@ -141,25 +198,31 @@ namespace HDeMods {
             config) {
             RoR2.Language.onCurrentLanguageChanged += ResetDescription;
         }
+
         [MethodImpl(MethodImplOptions.NoInlining | MethodImplOptions.NoOptimization)]
         public override void RegisterTokens() {
             Description = RoR2.Language.GetString(GetDescriptionToken());
         }
+
         [MethodImpl(MethodImplOptions.NoInlining | MethodImplOptions.NoOptimization)]
         private void ResetDescription() {
             Description = RoR2.Language.GetString(GetDescriptionToken());
         }
     }
+
     internal class LocalizedButtonOption : GenericButtonOption {
         [MethodImpl(MethodImplOptions.NoInlining | MethodImplOptions.NoOptimization)]
-        public LocalizedButtonOption(string name, string category, string description, string buttonText, UnityAction onButtonPressed) 
+        public LocalizedButtonOption(string name, string category, string description, string buttonText,
+            UnityAction onButtonPressed)
             : base(name, category, description, buttonText, onButtonPressed) {
             RoR2.Language.onCurrentLanguageChanged += ResetDescription;
         }
+
         [MethodImpl(MethodImplOptions.NoInlining | MethodImplOptions.NoOptimization)]
         public override void RegisterTokens() {
             Description = RoR2.Language.GetString(GetDescriptionToken());
         }
+
         [MethodImpl(MethodImplOptions.NoInlining | MethodImplOptions.NoOptimization)]
         private void ResetDescription() {
             Description = RoR2.Language.GetString(GetDescriptionToken());

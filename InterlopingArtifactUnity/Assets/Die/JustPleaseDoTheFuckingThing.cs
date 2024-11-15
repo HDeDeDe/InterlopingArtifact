afb7e1421e904da02a35c833b9a084634dc3e211
Ethan James Beal <21134925+HDeDeDe@users.noreply.github.com>
2024-11-15 14:59:43 -0800

    I hate unity with every fiber of my being.

--- InterlopingArtifactUnity/Assets/Die/JustPleaseDoTheFuckingThing.cs
+++ InterlopingArtifactUnity/Assets/Die/JustPleaseDoTheFuckingThing.cs
@@ -1,11 +1,8 @@
 ﻿#if UNITY_EDITOR
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 
-public class AssetBundleMaker : MonoBehaviour
-{
+public class JustPleaseDoTheFuckingThing : MonoBehaviour {
     [MenuItem ("Assets/AB Make")]
     static void BuildBundles()
     {

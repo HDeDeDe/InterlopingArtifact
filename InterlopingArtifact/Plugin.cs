faefe05386d81a2c965f45e169c84af1035a1f09
Ethan James Beal <21134925+HDeDeDe@users.noreply.github.com>
2025-05-26 17:36:33 -0700

    update version number

--- InterlopingArtifact/Plugin.cs
+++ InterlopingArtifact/Plugin.cs
@@ -19,7 +19,7 @@ namespace HDeMods {
         public const string PluginGUID = "com." + PluginAuthor + "." + PluginName;
         public const string PluginAuthor = "HDeDeDe";
         public const string PluginName = "InterlopingArtifact";
-        public const string PluginVersion = "0.3.1";
+        public const string PluginVersion = "0.4.0";
 
         public static InterlopingArtifactPlugin instance;
         public static bool startupSuccess = false;
